da0bf44366ab0ebc272fde7972a0c473540bd63c
Tomasposada26 <tomasposada26@gmail.com>
2025-11-03 18:37:56 -0500

    prueba 2

--- Codigo_base/Acentos/settings.py
+++ Codigo_base/Acentos/settings.py
@@ -13,7 +13,7 @@ load_dotenv(os.path.join(BASE_DIR, "openAI.env"))
 # Seguridad
 SECRET_KEY = 'django-insecure-reemplaza-esto-por-una-clave-real'
 DEBUG = True
-ALLOWED_HOSTS = ['3.95.64.11']
+ALLOWED_HOSTS = ['34.226.190.22']
 
 # Aplicaciones instaladas
 INSTALLED_APPS = [
